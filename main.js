09bd25332e448c3e944741a75411f84bd5eb6bd0
carey <schiafang@gmail.com>
2020-03-19 14:24:59 +0800

    A20 add todo-done-trash

--- main.js
+++ main.js
@@ -1,52 +1,100 @@
-// init
-let list = document.querySelector('#my-todo')
 const todos = ['Hit the gym', 'Read a book', 'Buy eggs', 'Organize office', 'Pay bills']
 
-for (let todo of todos) {
-  addItem(todo)
-}
-
-function addItem(text) {
-  let newItem = document.createElement('li')
-  newItem.innerHTML = `
-    <label for="todo">${text}</label>
-    <i class="delete fa fa-trash"></i>
-  `
-  list.appendChild(newItem)
-}
-
-//當 click 事件發生時，就取出 input#newTodo 裡的值，
-//並且呼叫 addItem()，把值塞進 template 裡，加入到 ul 裡去。
-
 const addBtn = document.querySelector('#addBtn')
+const input = document.querySelector('#newTodo')
 
+let todoList = document.querySelector('#my-todo')
+let doneList = document.querySelector('#my-done')
+let trashList = document.querySelector('#my-trash')
+
+for (let todo of todos) {
+  addTodoItem(todo)
+}
+
+///----- Listener -----///
+
+// 監聽add按鈕
 addBtn.addEventListener('click', function () {
-  console.log(this)
-  console.log(event.target)
   let inputValue = document.querySelector('#newTodo').value
-  // 如果 input 還沒有輸入內容，不會產生新的 todo
   if (inputValue.length >= 1) {
-    addItem(inputValue)
+    addTodoItem(inputValue)
+    input.value = ""
   }
 })
 
-//// Delete and Cheack
-list.addEventListener('click', function (event) {
-  console.log(this)
-  console.log(event.target)
-  //this這次監聽器綁定的對象是 ul#my-todo(list)
+// 監聽鍵盤input上按下enter輸入值
+input.addEventListener('keypress', function () {
+  let inputValue = document.querySelector('#newTodo').value
+  if (event.keyCode === 13) {
+    addTodoItem(inputValue)
+    input.value = ""
+  }
+})
 
-  ////加上if確保滑鼠點擊的元素是刪除按鈕
+// 監聽todoList上操作
+todoList.addEventListener('click', function (event) {
+  let li = event.target.parentElement
+  let text = event.target.parentElement.children[0].innerText
+
   if (event.target.classList.contains('delete')) {
-    let li = event.target.parentElement
+    li.remove()
+    addTrashItem(text)
+  } else if (event.target.classList.contains('check')) {
+    li.remove()
+    addDoneItem(text)
+  }
+})
+
+// 監聽doneList上操作
+doneList.addEventListener('click', function (event) {
+  let li = event.target.parentElement
+  let text = event.target.parentElement.children[0].innerText
+
+  if (event.target.classList.contains('delete')) {
+    li.remove()
+    addTrashItem(text)
+  } else if (event.target.classList.contains('undo')) {
+    li.remove()
+    addTodoItem(text)
+  }
+})
+
+// 監聽trashList上操作
+trashList.addEventListener('click', function (event) {
+  let li = event.target.parentElement
+
+  if (event.target.classList.contains('delete')) {
     li.remove()
   }
-  ////如果觸發事件的物件的tag name是LABEL，就套用.checked樣式。
-  else if (event.target.tagName === 'LABEL') {
-    event.target.classList.toggle('checked')
-  }
 })
-////當我們使用tagName來查詢標籤名稱時，回傳值會一律大寫，所以要寫成 LABEL
 
-// 當使用者在 input#newTodo 裡按下 Enter 鍵時，一樣可以新增 todo (提示：使用 keypress 事件，並且用 event.keyCode == 13 來鎖定 Enter 鍵)
 
+///----- Function -----///
+function addTodoItem(text) {
+  let todoItem = document.createElement('li')
+  todoItem.innerHTML = `
+    <label for="todo">${text}</label>
+    <i class="check fa fa-check-square-o" aria-hidden="true"></i>
+    <i class="delete fa fa-trash-o" aria-hidden="true"></i>
+  `
+  todoList.appendChild(todoItem)
+}
+
+function addDoneItem(text) {
+  let doneItem = document.createElement('li')
+  doneItem.innerHTML = `
+    <label for="done">${text}</label>
+    <i class="undo fa fa-undo" aria-hidden="true"></i>
+    <i class="delete fa fa-trash-o" aria-hidden="true"></i>
+  `
+  doneList.appendChild(doneItem)
+}
+
+function addTrashItem(text) {
+  let trashItem = document.createElement('li')
+  trashItem.innerHTML = `
+    <label for="trash">${text}</label>
+    <i class="delete fa fa-trash-o" aria-hidden="true"></i>
+  `
+  trashList.appendChild(trashItem)
+}
